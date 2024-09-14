a3a7157797ca3fd0262a6795a3ae351bff04126e
pmalimba <pmalimba@yahoo.com>
2024-09-14 15:40:31 +0200

    Added the Los Angeles date element in javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
+losAngelesDateElement.innerHTML = "September 13th 2024";
+losAngelesTimeElement.innerHTML = "15:36:12 <small>PM</small>";
