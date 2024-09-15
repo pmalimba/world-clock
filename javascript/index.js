c7b54cb6ed389b2529705c61342f934a1a8d5fcd
pmalimba <pmalimba@yahoo.com>
2024-09-15 14:56:29 +0200

    Recreated the html, CSS and javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,3 @@
 let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-losAngelesDateElement.innerHTML = "September 13th 2024";
-losAngelesTimeElement.innerHTML = "15:36:12 <small>PM</small>";
+losAngelesElement.innerHTML = "hI";
