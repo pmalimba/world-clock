bc70b30ff31053458896f77d412ce82ddbecabd6
pmalimba <pmalimba@yahoo.com>
2025-04-09 23:21:01 +0200

    Updated the data for the cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,25 @@
-let theHagueElement = document.querySelector("#the-hague");
-let theHagueDateElement = theHagueElement.querySelector(".date");
-let theHagueTimeElement = theHagueElement.querySelector(".time");
+function updateTime() {
+  //the hague
+  let theHagueElement = document.querySelector("#the-hague");
+  let theHagueDateElement = theHagueElement.querySelector(".date");
+  let theHagueTimeElement = theHagueElement.querySelector(".time");
+  let theHagueTime = moment().tz("Europe/Amsterdam");
 
-theHagueDateElement.innerHTML = "April 9th 2025";
-theHagueTimeElement.innerHTML = "19:03:25<small>PM</small>";
+  theHagueDateElement.innerHTML = theHagueTime.format("MMMM Do YYYY");
+  theHagueTimeElement.innerHTML = theHagueTime.format(
+    "h:mm:ss[<small>]P[</small>]"
+  );
+
+  //auckland
+  let aucklandElement = document.querySelector("#auckland");
+  let aucklandDateElement = aucklandElement.querySelector(".date");
+  let aucklandTimeElement = aucklandElement.querySelector(".time");
+  let aucklandTime = moment().tz("Pacific/Auckland");
+
+  aucklandDateElement.innerHTML = aucklandTime.format("MMMM Do YYYY");
+  aucklandTimeElement.innerHTML = aucklandTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
