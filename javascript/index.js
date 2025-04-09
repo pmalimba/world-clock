43c72813d7d37da7fcf34231bb3f49720653a5b9
pmalimba <pmalimba@yahoo.com>
2025-04-09 20:10:44 +0200

    Styled project with CSS

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,6 @@
+let theHagueElement = document.querySelector("#the-hague");
+let theHagueDateElement = theHagueElement.querySelector(".date");
+let theHagueTimeElement = theHagueElement.querySelector(".time");
 
+theHagueDateElement.innerHTML = "April 9th 2025";
+theHagueTimeElement.innerHTML = "19:03:25<small>PM</small>";
